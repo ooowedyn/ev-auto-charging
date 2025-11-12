959a589979574fad5cc2a5c0e96dba337acfa9fc
suyeon-jung <ooowedyn@naver.com>
2025-11-12 23:25:27 +0900

    251112

--- vision/EVCI/yolov11_val.py
+++ vision/EVCI/yolov11_val.py
@@ -2,14 +2,14 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolo11s.pt")  # load an official model
-model = YOLO("runs/EVCI_train/yolo11s_setA/weights/best.pt")  # load a custom model
+model = YOLO("runs/EVCI_train/yolo11s_setA2/weights/best.pt")  # load a custom model
 
 # Validate the model
 metrics = model.val()  # no arguments needed, dataset and settings remembered
-metrics.box.map  # map50-95
-metrics.box.map50  # map50
-metrics.box.map75  # map75
-metrics.box.maps  # a list contains map50-95 of each category
+# metrics.box.map  # map50-95
+# metrics.box.map50  # map50
+# metrics.box.map75  # map75
+# metrics.box.maps  # a list contains map50-95 of each category
 
 # Run batched inference on a list of images
 results = model(["datasets/EVCI/EVCI_B_set_Test/img/BoltEV_White_2019_Rainy_Time(20200722)_Morning_OutsideB_DC_Off_color002.png",
